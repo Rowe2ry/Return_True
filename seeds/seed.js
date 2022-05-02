ae5364b23b23aea307e51267e05b87f47c8d5513
Chris Rowe <rowe2ry@yahoo.com>
2022-05-01 22:37:25 -0400

    Added seeding script.

---
+++ seeds/seed.js
@@ -0,0 +1,24 @@
+const sequelize = require('../config/connection');
+const { seedUsers } = require('./userSeeds');
+const { seedPosts } = require('./blogPostSeeds');
+const { seedComments } = require('./commentSeeds');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+
+  await sequelize.sync({ force: true });
+  console.log('\n----- DATABASE SYNCED -----\n');
+  await seedUsers();
+  console.log('\n----- USERS SEEDED -----\n');
+
+  await seedPosts();
+  console.log('\n----- BLOG POSTS SEEDED -----\n');
+
+  await seedComments();
+  console.log('\n----- Comments SEEDED -----\n');
+
+
+  process.exit(0);
+};
+
+seedAll();
